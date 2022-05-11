bdf73bb6fc91e8d7a32e692a92985de5e9eb2911
Gracie Devine <gracie.devine@gmail.com>
2022-05-11 10:15:52 -0400

    file can be saved as .json and .xlsx.

--- no-browser.py
+++ no-browser.py
@@ -1,8 +1,16 @@
 import pandas as pd
 from pprint import PrettyPrinter
 
+
 pp = PrettyPrinter(indent=4)
 df = pd.read_html("table.html")[0]
-print(df)
 
-pp.pprint(df[["Server Name", "Host", "Port"]].to_json(orient="index", index=False))
+pp.pprint(
+    df[["Server Name", "Host", "Port"]].to_json(
+        "./ftp-table.json", orient="table", index=False
+    )
+)
+
+writer = pd.ExcelWriter("ftp-table.xlsx", engine="xlsxwriter")
+df.to_excel(writer, index=False)
+writer.save()
